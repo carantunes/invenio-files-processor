04b29514686f24644a62260ededbbc677f0c927b
Carina Antunes <carinadeoliveiraantunes@gmail.com>
2020-06-30 12:18:56 +0100

    add example to signal doc

--- invenio_files_processor/signals.py
+++ invenio_files_processor/signals.py
@@ -14,5 +14,20 @@ _signals = Namespace()
 
 file_processed = _signals.signal('file-processed')
 """File processed signal.
-Sent when a file is processed.
+
+When implementing the event listener, the result can be retrieved from `data`.
+Example event listener (subscriber) implementation:
+.. code-block:: python
+    def listener(sender, processor_id, file, data):
+        \"""File processor listener.
+
+        :param sender: current_app.
+        :param processor_id: str processor id.
+        :param file: ObjectVersion input.
+        :param data: Data array with keys metadata and content.
+        \"""
+        # do something with the data
+
+    from invenio_files_processor.signals import file_processed
+    file_processed.connect(listener)
 """
